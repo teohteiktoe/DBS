387a4498b7ebc7522b2a97b1efca79e1baf35283
Teoh TeikToe <ttt@mensa.org.sg>
2023-10-26 07:47:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 from flask import Flask, request, render_template
-
 app = Flask(__name__)
-
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
@@ -15,12 +7,8 @@ def index():
         return(render_template("index.html", result=90.22 + (-50.60 * rate)))
     else:
         return(render_template("index.html", result="waiting......."))
-
 if __name__ == "__main__":
     app.run()
-
-
-# In[ ]:
 
 
 
